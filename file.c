0b1e817fe866fbd466d772ab246989a78ba3c04f
tb7599 <31754957+tb7599@users.noreply.github.com>
2017-12-20 18:54:32 +0000

    Update file.c

--- file.c
+++ file.c
@@ -5,3 +5,6 @@ It's an honour to have you in front of me.
 I've been waiting for this day since my childhood.
 You are my idol,my inspiration as well as my role model.
 I want to become just like you.
+I don't know why but this feeling arose when I first saw you with my quite childish
+but maturity filled eyes.
+But this is a fact that I can't become exactly like you in my life.
