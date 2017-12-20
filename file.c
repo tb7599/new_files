06f7d1bb8574390e59bb2b8231110480a45f113f
tb7599 <31754957+tb7599@users.noreply.github.com>
2017-12-20 18:50:37 +0000

    Update file.c

--- file.c
+++ file.c
@@ -3,3 +3,5 @@ Tanishka Bhardwaj this side.
 I'm really glad to meet you.
 It's an honour to have you in front of me.
 I've been waiting for this day since my childhood.
+You are my idol,my inspiration as well as my role model.
+I want to become just like you.
